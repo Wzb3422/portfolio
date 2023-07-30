a00ed0f363c56c2e7fae918f2c10d81566ba013d
Zeb Wu <wzb3422@gmail.com>
2023-07-31 02:10:17 +0800

    feat: remove parts

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -53,49 +53,6 @@ const HomePage = async () => {
 	return (
 		<AnimatePage>
 			<HeroSection />
-
-			<section id="experience">
-				<Container>
-					<h2 className="headline mb-4 mt-12 text-4xl">Experience</h2>
-					<WorkExperience jobs={jobs} />
-				</Container>
-			</section>
-
-
-			<section id="tools" className="my-40">
-				<Container>
-					<h2 className="headline mt-24 text-center text-xl md:text-2xl lg:text-3xl">
-						Some of my favourite tools
-					</h2>
-
-					<div className="mx-auto mt-8 flex max-w-5xl flex-wrap items-center justify-center gap-x-16 gap-y-8">
-						<LogoNext className="w-32" aria-label="Next.js" />
-						<LogoSvelte className="w-12" aria-label="SvelteKit" />
-						<LogoHygraph className="w-28 md:w-36" aria-label="Hygraph" />
-						<LogoSanity className="w-32 md:w-40" aria-label="Sanity" />
-						<LogoStorybook className="w-32 md:w-40" aria-label="Storybook" />
-						<LogoGraphql className="w-28 md:w-36" aria-label="GraphQL" />
-						<LogoVercel className="w-32 md:w-36" aria-label="Vercel" />
-						<LogoFigma className="w-8" aria-label="Figma" />
-						<LogoCypress className="w-32 md:w-40" aria-label="Cypress" />
-					</div>
-				</Container>
-			</section>
-			<section id="services">
-				<Container>
-					<h2 className="headline mt-20 text-2xl md:text-4xl lg:text-5xl">
-						Services I provide
-						<span className="mt-2 block text-xl font-bold text-off-black dark:text-off-white md:text-2xl">
-							as a software architect with a passion for creating applications
-							and websites that people love.
-						</span>
-					</h2>
-
-					{services.map((service, i) => (
-						<Service key={service.slug} {...service} isReversed={i % 2 !== 0} />
-					))}
-				</Container>
-			</section>
 		</AnimatePage>
 	);
 };
